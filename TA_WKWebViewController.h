4e48eec28db34e2b7d296f7cbde1a61df7d8239c
seanboler <zhangjiaqi_125@sina.com>
2020-07-22 14:37:42 +0800

    Update TA_WKWebViewController.h

--- TA_WKWebViewController.h
+++ TA_WKWebViewController.h
@@ -1,9 +1,8 @@
 //
 //  TA_WKWebViewController.h
-//  TA_5youshenghuoApp
 //
 //  Created by 张家旗 on 2019/9/11.
-//  Copyright © 2019 com.tlkj. All rights reserved.
+//  Copyright © 2019 com.zjq. All rights reserved.
 //
 
 #import "BaseViewController.h"
@@ -11,14 +10,17 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TA_WKWebViewController : BaseViewController 
-
+  
 ///网页url
 @property (nonatomic,copy)NSString *urlStr;
 ///网页返回处理
-@property (nonatomic,copy)void(^backBlock)();
+@property (nonatomic,copy)void(^backBlock)(void);
 ///o判断是否需要navgation 导航 并设置layout
 @property (nonatomic, assign) BOOL HaveNavHeaher;
-
+///html 网页内容
+@property (nonatomic, strong) NSString *HTMLString;
+///导航标题
+@property (nonatomic, copy) NSString *itemTitle;
 
 @end
 
